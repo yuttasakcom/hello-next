917e7adac585ef2f5575f3871b9fb8ddfcce1bcc
Yuttasak Pannawat <yuttasakcom@gmail.com>
2018-06-04 14:36:32 +0700

    create project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// This is the Link API
+import Link from 'next/link'
+
+import Layout from '../components/MyLayout.js'
+
+const Index = () => (
+  <Layout>
+    <p>Hello Next.js</p>
+  </Layout>
+)
+
+export default Index
